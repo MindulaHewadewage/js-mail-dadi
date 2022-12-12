55b98fce7d428ebd933cb52f09b85d7dd313484c
MindulaHewadewage <mindufernando@gmail.com>
2022-12-12 22:43:58 +0100

    struttura primo es

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-const targetElement = document.getElementById('target')
+const targetElement = document.getElementById('target');
+const buttonElement = document.getElementById('button');
+let userBet = document.getElementById('user-bet');
+let  cpuBet = document.getElementById('cpu-bet');
+let winner = document.getElementById('winner')
 
 
-let randomNumber = Math.floor(Math.random() * 6) + 1;
-console.log(randomNumber);
+buttonElement.addEventListener('click', function(){
+
+    let randomNumberUser = Math.floor(Math.random() * 6) + 1;
+    userBet.innerText = 'Numero :' + randomNumberUser;
+    console.log(randomNumberUser);
+
+    let randomNumberCpu = Math.floor(Math.random() * 6) + 1;
+    cpuBet.innerText = 'Numero :' + randomNumberCpu;
+    console.log(randomNumberCpu);
+
+
+    let winner = '';
+
+    if (randomNumberUser === randomNumberCpu){
+        winner = 'Pareggio!';
+    } 
+
+    else if (randomNumberUser > randomNumberCpu){
+        winner = 'Hai vinto User!';
+    }
+    
+    else if (randomNumberUser < randomNumberCpu){
+        winner = 'Hai vinto CPU!';
+    }
+
+    
+
+    console.log(winner);
+})
+
+
+winner.innerText += 'winner';
