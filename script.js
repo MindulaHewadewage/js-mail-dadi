be187f0a983431109869a504f2227898abb7b0e8
MindulaHewadewage <mindufernando@gmail.com>
2022-12-13 11:16:32 +0100

    correzzione esercizio

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-// const targetElement = document.getElementById('target');
-// const buttonElement = document.getElementById('button');
-// let userBet = document.getElementById('user-bet');
-// let  cpuBet = document.getElementById('cpu-bet');
-// let winner = document.getElementById('winner')
+const targetElement = document.getElementById('target');
+const buttonElement = document.getElementById('button');
+let userBet = document.getElementById('user-bet');
+let  cpuBet = document.getElementById('cpu-bet');
+let winner = document.getElementById('winner')
 
 
-// buttonElement.addEventListener('click', function(){
+buttonElement.addEventListener('click', function(){
 
-//     let randomNumberUser = Math.floor(Math.random() * 6) + 1;
-//     userBet.innerText = 'Numero :' + randomNumberUser;
-//     console.log(randomNumberUser);
+    let randomNumberUser = Math.floor(Math.random() * 6) + 1;
+    userBet.innerText = 'Numero :' + randomNumberUser;
+    console.log(randomNumberUser);
 
-//     let randomNumberCpu = Math.floor(Math.random() * 6) + 1;
-//     cpuBet.innerText = 'Numero :' + randomNumberCpu;
-//     console.log(randomNumberCpu);
+    let randomNumberCpu = Math.floor(Math.random() * 6) + 1;
+    cpuBet.innerText = 'Numero :' + randomNumberCpu;
+    console.log(randomNumberCpu);
 
 
-//     let winner = '';
+    let winner = '';
 
-//     if (randomNumberUser === randomNumberCpu){
-//         winner = 'Pareggio!';
-//     } 
+    if (randomNumberUser === randomNumberCpu){
+        winner = 'Pareggio!';
+    } 
 
-//     else if (randomNumberUser > randomNumberCpu){
-//         winner = 'Hai vinto User!';
-//     }
+    else if (randomNumberUser > randomNumberCpu){
+        winner = 'Hai vinto User!';
+    }
     
-//     else if (randomNumberUser < randomNumberCpu){
-//         winner = 'Hai vinto CPU!';
-//     }
+    else if (randomNumberUser < randomNumberCpu){
+        winner = 'Hai vinto CPU!';
+    }
 
     
 
-//     console.log(winner);
-// })
+    console.log(winner);
+})
 
 
-// winner.innerText += 'winner';
+winner.innerText += 'winner';
--- script2.js
+++ script2.js
@@ -10,8 +10,27 @@ const validEmails  = [
 // prendiamo documenti necessari dal dom
 const email = document.getElementById('email');
 const button = document.getElementById('button');
+const result = document.getElementById('result');
 
 
+// Event listener
+button.addEventListener('click', function(){
+    const userEmail = email.ariaValueMax.trim();
+ 
+    let isAllowed = false;
+
+    // ciclo for
+    for(let i = 0; i < validEmails.length; i++) {
+        const currentEmail = validEmails[i];
+        console.log(currentEmail, userEmail);
+
+        if (currentEmail = validEmails){
+            isAllowed = true;
+        }
+    }
+
+    result.innerText = isAllowed ? 'Benvenuto' : 'Spiacente, non sei autorizzato';
 
 
+})
 
