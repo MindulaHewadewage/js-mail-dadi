329b9569700808f8c66d6b67397da7ad29fda4f4
MindulaHewadewage <mindufernando@gmail.com>
2022-12-13 09:34:10 +0100

    dichiaro documenti dal dom

--- script2.js
+++ script2.js
@@ -1,1 +1,17 @@
-console.log('JS OK');
+
+// Email Valide
+const validEmails  = [
+    'mindufernando@gmail.com',
+    'marcorossi@gmail.com',
+    'filippobianchi@gmail.com',
+    'lorenzoverdi@gmail.com'
+];
+
+// prendiamo documenti necessari dal dom
+const email = document.getElementById('email');
+const button = document.getElementById('button');
+
+
+
+
+
